154a54d8ebafef4cc8b705f18379f85ca2a64f0f
bargholz eimii <818_77_@ezweb.ne.jp>
2022-12-02 15:33:40 +0900

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  resources :dogs do
+    collection do
+      get 'search'
+      get 'search_results'
+    end
+  end
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  resources :user_choices
+
+  resources :user_steps
 end
