e3115b60ebe8a3234c55d9736a95f5b5cc28e383
bargholz eimii <818_77_@ezweb.ne.jp>
2022-12-02 15:15:28 +0900

    コントローラー作成

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,26 @@
+class DogsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+    
+  end
+
+  def show
+
+  end
+
+  def search
+
+  end
+
+  def search_results
+
+  end
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+  
+end
