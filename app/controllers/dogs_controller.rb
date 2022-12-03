7539e17790a0ec456eb24f19cf6f374e5b782b32
bargholz eimii <818_77_@ezweb.ne.jp>
2022-12-03 17:06:46 +0900

    一覧ページ

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,18 +1,19 @@
 class DogsController < ApplicationController
   def index
-
+    @dogs = Dog.all
   end
 
   def new
-
+    @dog = Dog.new
   end
 
   def create
-    
+    Dog.create(dog_params)
+    redirect_to dogs_url
   end
 
   def show
-
+    @dog = Dog.find(params[:id])
   end
 
   def search
@@ -21,6 +22,12 @@ class DogsController < ApplicationController
 
   def search_results
 
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :size, :vehicle, :cleaning, :active, :exercise, :home, :house, :image_url)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require "csv"
 
 CSV.foreach('db/sample_list.csv', headers: true) do |row|
   Dog.create(
-    name:      row[0].to_i,
+    name:      row[0],
     size:      row[1].to_i,
     vehicle:   row[2].to_i,
     cleaning:  row[3].to_i,
@@ -10,6 +10,6 @@ CSV.foreach('db/sample_list.csv', headers: true) do |row|
     exercise:  row[5].to_i,
     home:      row[6].to_i,
     house:     row[7].to_i,
-    image_url: row[8].to_i
+    image_url: row[8]
   )
 end
