31af6a3b787bbad6e66abb9b21564810810bc43b
bargholz eimii <818_77_@ezweb.ne.jp>
2022-12-05 16:25:25 +0900

    admin作成

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'ransack'
 gem 'kaminari'
+gem 'rails_admin'
+gem 'cancancan'
--- app/controllers/user_choices_controller.rb
+++ app/controllers/user_choices_controller.rb
@@ -8,20 +8,7 @@ class UserChoicesController < ApplicationController
   end
 
   def create
-    @user_choices = UserChoice.new(
-      vehicle: session[:vehicle],
-      cleaning: session[:cleaning],
-      active: session[:active],
-      exercise: session[:exercise],
-      home: session[:last_name_kana], 
-      house: session[:house]
-    )
-    if @user_choices.save
-      session[:id] = @user.id
-      redirect_to user_choices_path
-    else
-      render '/signup/registration'
-    end
+    @user_choice = UserChoice.create(user_choice_params)
   end
 
   def edit
