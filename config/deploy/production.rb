2135afc9df4fc475083f0745df9435f2ba420db3
bargholz eimii <818_77_@ezweb.ne.jp>
2022-12-12 14:26:46 +0900

    x86_64-linux追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,2 @@
-server "52.69.21.96",
-  user: "app",
-  roles: %w{web app},
-  ssh_options: {
-    keys: %w(/Users/kudoeimii.ssh/id_rsa),
-  }
+server "52.69.21.96", user: "app", roles: %w{web app}
+set :ssh_options, keys: '/Users/kudoeimii/.ssh/id_rsa'
